d8acf47cebedd1b092dfeb75ea1a3a6ceb5a7873
Brayan-Salazarr <brayansalazzar@gmail.com>
2025-09-15 17:31:47 -0500

    Agrego implementacion de IF

--- BlogTecnologico/src/app/inicio-sesion/inicio-sesion.ts
+++ BlogTecnologico/src/app/inicio-sesion/inicio-sesion.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Nav } from '../shared/components/nav/nav';
 import { Footer } from '../shared/components/footer/footer';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-sesion',
-  imports: [Nav,Footer],
+  imports: [Nav,Footer, RouterModule],
   templateUrl: './inicio-sesion.html',
   styleUrl: './inicio-sesion.css'
 })
 export class InicioSesion {
+protected readonly mostrarContra = signal(true);
 
+toggleElemento() {
+    this.mostrarContra.update(valor => !valor);
+  }
 }
--- BlogTecnologico/src/app/registro/registro.ts
+++ BlogTecnologico/src/app/registro/registro.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Nav } from '../shared/components/nav/nav';
 import { Footer } from '../shared/components/footer/footer';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
-  imports: [Nav,Footer],
+  imports: [Nav,Footer, RouterModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css'
 })
 export class Registro {
+  protected readonly mostrarContra = signal(true);
+  protected readonly mostrarContra2 = signal(true);
 
+ toggleElemento() {
+    this.mostrarContra.update(valor => !valor);
+  }
+
+  toggleElemento2() {
+    this.mostrarContra2.update(valor => !valor);
+  }
 }
