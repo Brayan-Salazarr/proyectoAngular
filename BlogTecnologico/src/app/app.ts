f43125165aa3c5a1fade620c5bc7c5184a075ce2
Brayan-Salazarr <brayansalazzar1221@gmail.com>
2025-09-17 22:05:01 -0500

    Agrego for y una parte del ngClass

--- BlogTecnologico/src/app/app.ts
+++ BlogTecnologico/src/app/app.ts
@@ -1,12 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('BlogTecnologico');
+
+  protected readonly modoOscuro = signal(false);
+
+   toggleOscuro() {
+    this.modoOscuro.update(valor => !valor);
+  }
 }
--- BlogTecnologico/src/app/imagenes/imagenes.ts
+++ BlogTecnologico/src/app/imagenes/imagenes.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Nav } from '../shared/components/nav/nav';
 import { Header } from '../shared/components/header/header';
 import { Footer } from '../shared/components/footer/footer';
+
+interface Imagen{
+  img: String
+}
 
 @Component({
   selector: 'app-imagenes',
@@ -10,5 +14,31 @@ import { Footer } from '../shared/components/footer/footer';
   styleUrl: './imagenes.css'
 })
 export class Imagenes {
+  protected readonly imagenes = signal<Imagen[]>([]);
+
+  constructor(){
+    this.cargarImagenes();
+  }
+
+  private cargarImagenes(): void{
+    const listaImagenes: Imagen[]=[
+      {
+        img: 'assets/images/imagen1.png'
+      },
+      {
+        img: 'assets/images/images 3.png'
+      },
+      {
+        img: 'assets/images/images 4.png'
+      },
+      {
+        img: 'assets/images/images 5.png'
+      },
+      {
+        img: 'assets/images/images 6.png'
+      }
+    ];
+    this.imagenes.set(listaImagenes);
+  }
 
 }
