d8dd5d87952ac41df60e87ed9e4920cc1c3dc2db
Brayan-Salazarr <brayansalazzar1221@gmail.com>
2025-09-18 21:33:20 -0500

    Agrego switch

--- BlogTecnologico/src/app/shared/components/imgm-movi/imgm-movi.ts
+++ BlogTecnologico/src/app/shared/components/imgm-movi/imgm-movi.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-imgm-movi',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './imgm-movi.css'
 })
 export class ImgmMovi {
-
+   protected readonly imagen = signal<'imagen1' | 'imagen2' | 'imagen3' | 'imagen4' | 'imagen5'>('imagen1');
+  
+  cambiarImagen() {
+    if (this.imagen() == 'imagen1') {
+      this.imagen.set('imagen2');
+    } else if (this.imagen() == 'imagen2') {
+      this.imagen.set('imagen3');
+    } else if (this.imagen() == 'imagen3'){
+      this.imagen.set('imagen4');
+    }else if (this.imagen() == 'imagen4'){
+      this.imagen.set('imagen5');
+    }else{
+      this.imagen.set('imagen1');
+    }
+  }
 }
