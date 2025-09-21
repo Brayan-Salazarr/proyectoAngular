efc800988e9d5d8858aee61b6a40f0913a411421
Brayan-Salazarr <brayansalazzar1221@gmail.com>
2025-09-21 12:00:31 -0500

    Agrego modo oscuro con ngCLass

--- BlogTecnologico/src/app/app.ts
+++ BlogTecnologico/src/app/app.ts
@@ -1,19 +1,17 @@
 import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from './shared/components/nav/nav';
+import { ThemeService } from './themeService';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgClass],
+  imports: [RouterOutlet, NgClass,Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('BlogTecnologico');
 
-  protected readonly modoOscuro = signal(false);
-
-   toggleOscuro() {
-    this.modoOscuro.update(valor => !valor);
-  }
+  constructor(public themeService: ThemeService) {}
 }
--- BlogTecnologico/src/app/shared/components/nav/nav.ts
+++ BlogTecnologico/src/app/shared/components/nav/nav.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ThemeService } from '../../../themeService';
 
 @Component({
   selector: 'app-nav',
@@ -8,5 +9,9 @@ import { RouterModule } from '@angular/router';
   styleUrl: './nav.css'
 })
 export class Nav {
+  constructor(public themeService: ThemeService) {}
 
+  toggleTheme(): void {
+    this.themeService.toggleOscuro();
+  }
 }
